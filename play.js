82a64262d5686ec65f3b8c1bdd1a789d7d637782
Rémi Vanicat <vanicat@debian.org>
2016-04-16 22:46:38 +0200

    Using this more often...

--- play.js
+++ play.js
@@ -4,55 +4,48 @@
 * @licence            {@link https://creativecommons.org/publicdomain/zero/1.0/deed.fr|CC0 1.0}
 */
 
-
-var player;
-var walls;
-var shifting;
-var exit;
-var map;
-var killing;
-var fire;
 var cursors;
-var accelerator = 0;
 const accel_go  = 10;
 const square_speed = 200;
 
 const square = {
     frame: 1,
     moving: function () {
-        if (! player.body.touching.none)
+        if (! this.body.touching.none)
         {
-            player.body.velocity.x = 0;
-            player.body.velocity.y = 0;
+            this.body.velocity.x = 0;
+            this.body.velocity.y = 0;
         }
-        if(player.body.velocity.x == 0 && player.body.velocity.y == 0)
+        if(this.body.velocity.x == 0 && this.body.velocity.y == 0)
         {
             if (cursors.left.isDown)
             {
                 //  Move to the left
-                player.body.velocity.x = -square_speed;
+                this.body.velocity.x = -square_speed;
             }
             if (cursors.right.isDown)
             {
-                player.body.velocity.x += +square_speed;
+                this.body.velocity.x += +square_speed;
             }
 
             if (cursors.up.isDown)
             {
                 // up
-                player.body.velocity.y = -square_speed;
+                this.body.velocity.y = -square_speed;
             }
             if (cursors.down.isDown)
             {
-                player.body.velocity.y += +square_speed;
+                this.body.velocity.y += +square_speed;
             }
         }
     },
     setup: function () {
-        //  Player physics properties.
-        player.body.bounce.y = 0;
-        player.body.bounce.x = 0;
-        player.body.gravity.y = 0;
+        //  This physics properties.
+        this.body.bounce.y = 0;
+        this.body.bounce.x = 0;
+        this.body.gravity.y = 0;
+        this.body.velocity.x = 0;
+        this.body.velocity.y = 0;
     }
 };
 
@@ -62,85 +55,89 @@ const round = {
         if (cursors.left.isDown)
         {
             //  Move to the left
-            player.body.velocity.x += -1;
-            if (accelerator > 0) accelerator = 0;
-            else if (accelerator < -accel_go)
-                player.body.velocity.x += -5;
-            else accelerator--;
+            this.body.velocity.x += -1;
+            if (this.accelerator > 0) this.accelerator = 0;
+            else if (this.accelerator < -accel_go)
+                this.body.velocity.x += -5;
+            else this.accelerator--;
         }
         else if (cursors.right.isDown)
         {
-            player.body.velocity.x += 1;
-            if (accelerator < 0) accelerator = 0;
-            else if (accelerator > -accel_go)
-                player.body.velocity.x += 5;
-            else accelerator++;
+            this.body.velocity.x += 1;
+            if (this.accelerator < 0) this.accelerator = 0;
+            else if (this.accelerator > -accel_go)
+                this.body.velocity.x += 5;
+            else this.accelerator++;
         }
 
-        //  Allow the player to jump if they are touching the ground.
-        if (cursors.up.isDown && player.body.blocked.down)
+        //  Allow the this to jump if they are touching the ground.
+        if (cursors.up.isDown && this.body.blocked.down)
         {
-            player.body.velocity.y += -100;
+            this.body.velocity.y += -100;
         }
     },
     setup: function () {
         //  Player physics properties.
-        player.body.bounce.y = 0.9;
-        player.body.bounce.x = 0.9;
-        player.body.gravity.y = 300;
+        this.body.bounce.y = 0.9;
+        this.body.bounce.x = 0.9;
+        this.body.gravity.y = 300;
+        this.body.velocity.x = 0;
+        this.body.velocity.y = 0;
+        this.accelerator = 0;
     }
 };
 
-function shapeshift(form) {
-    player.frame = form.frame;
-    player.moving = form.moving;
-    form.setup();
-}
-
 var playState = {
+    shapeshift: function (form) {
+        this.player.frame = form.frame;
+        this.player.moving = form.moving;
+        this.player.setup = form.setup;
+        this.player.setup();
+    },
+
     create: function() {
         //  A simple background for our game
         background.addToWorld();
 
-        map = game.add.tilemap('level1');
+        var map = game.add.tilemap('level1');
         map.addTilesetImage('wallTile', 'gameTiles');
 
         map.setCollisionBetween(1, 16, true, 'walls');
 
         //create layer
-        walls = map.createLayer('walls');
-        walls.resizeWorld();
+        this.walls = map.createLayer('walls');
+        this.walls.resizeWorld();
         // walls.debug = true;
 
         // Shifting stuff
-        shifting = game.add.group();
+        this.shifting = game.add.group();
         for(var i = 0; i < 7; i++)
         {
-            map.createFromObjects('objects', 18 + i , 'objects', i, true, false, shifting);
+            map.createFromObjects('objects', 18 + i , 'objects', i, true, false, this.shifting);
         }
 
         // exit
-        exit = game.add.group();
-        exit.enableBody = true;
-        map.createFromObjects('objects', 17, 'objects', 7, true, false, exit);
+        this.exit = game.add.group();
+        this.exit.enableBody = true;
+        map.createFromObjects('objects', 17, 'objects', 7, true, false, this.exit);
 
         // Dangerous stuff
 
-        killing = game.add.group();
-        killing.enableBody = true;
+        this.killing = game.add.group();
+        this.killing.enableBody = true;
 
-        map.createFromObjects('objects', 25, 'objects', 8, true, false, exit);
+        map.createFromObjects('objects', 25, 'objects', 8, true, false, this.exit);
 
         // The player and its settings
-        player = game.add.sprite(32, game.world.height - 150, 'rolling');
+        this.player = game.add.sprite(32, game.world.height - 150, 'rolling');
 
         //  We need to enable physics on the player
-        game.physics.arcade.enable(player);
-        player.body.collideWorldBounds = true;
+        game.physics.arcade.enable(this.player);
+        this.player.body.collideWorldBounds = true;
 
-        shapeshift(round);
+        this.shapeshift(round);
 
-        game.camera.follow(player); // Mmm...
+        game.camera.follow(this.player); // Mmm...
 
         //  Our controls.
         cursors = game.input.keyboard.createCursorKeys();
@@ -148,18 +145,18 @@ var playState = {
 
     update: function() {
         //  Collide the player and the stars with the wall
-        game.physics.arcade.collide(player, walls);
+        game.physics.arcade.collide(this.player, this.walls);
 
-        if (game.physics.arcade.collide(player, killing))
+        if (game.physics.arcade.collide(this.player, this.killing))
         {
             game.state.start('menu');
         }
 
-        if (game.physics.arcade.overlap(player, exit))
+        if (game.physics.arcade.overlap(this.player, this.exit))
         {
             game.state.start('menu');
         }
 
-        player.moving();
+        this.player.moving();
     }
 };
